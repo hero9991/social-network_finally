61464a7eeae19e217f5a106027855f636a6717e0
Mger <vanyakubarin@mail.ru>
2020-11-03 23:18:12 +0300

    Your awesome message

---
+++ build/precache-manifest.87fedb3f0bcff88ed775db49c00c1e25.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c8d37ba0d10dda902e2e8ce4626620",
+    "url": "/soc_app/index.html"
+  },
+  {
+    "revision": "6410a3fa8040a00b1eab",
+    "url": "/soc_app/static/css/main.fa661b55.chunk.css"
+  },
+  {
+    "revision": "e3f7db9bb3f16e00a2f1",
+    "url": "/soc_app/static/js/2.fa129b5a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/soc_app/static/js/2.fa129b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6410a3fa8040a00b1eab",
+    "url": "/soc_app/static/js/main.89b3641d.chunk.js"
+  },
+  {
+    "revision": "02b4a4ec379fc0901579",
+    "url": "/soc_app/static/js/runtime-main.54bdce48.js"
+  },
+  {
+    "revision": "74cd23b48421280be0756c814555260a",
+    "url": "/soc_app/static/media/avatar.74cd23b4.png"
+  },
+  {
+    "revision": "c28a8c63a41111c185b738be5e89ed72",
+    "url": "/soc_app/static/media/logo.c28a8c63.png"
+  },
+  {
+    "revision": "5861c44db520a839676b8df01f7c2213",
+    "url": "/soc_app/static/media/preloader.5861c44d.gif"
+  }
+]);
